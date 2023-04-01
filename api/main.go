a81e44f7bf25f8c4336d6713f0dd40a4cdf02a98
TheMrPuffin <garnera99@gmail.com>
2023-04-01 15:30:59 +0100

    Setting up initial Golang files

---
+++ api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Define your API routes and handlers here
+
+	r.Run() // Start the HTTP server
+}
