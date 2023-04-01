2ac914726cd4e0d64615067121b76595352e0b50
TheMrPuffin <garnera99@gmail.com>
2023-04-01 18:30:11 +0100

    Adding basic ping router for testing

--- api/main.go
+++ api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +10,13 @@ func main() {
 	// Create router with default middleware
 	router := gin.Default()
 
+	router.GET("/ping", ping)
+
 	router.Run() // Start the HTTP server
+}
+
+func ping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
