dc0d0c321208b620a13749c781083894549f7264
TheMrPuffin <garnera99@gmail.com>
2023-04-16 14:29:55 +0100

    Adding CORs middleware to allow all origins

--- api/main.go
+++ api/main.go
@@ -4,12 +4,18 @@ import (
 	"github.com/TheMrPuffin/linkdex/configs"
 	"github.com/TheMrPuffin/linkdex/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	// Create router with default middleware
 	router := gin.Default()
+
+	// COR configuration for GIN router
+	// Currently allows all origins
+	router.Use(cors.Default()) // This needs to be before routes declaration
 
 	// Run database
 	configs.ConnectDb()
