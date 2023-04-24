b545858f0d4c96b8bdd695eb07bee01cf5a5e89b
TheMrPuffin <garnera99@gmail.com>
2023-04-24 21:05:13 +0100

    Adding simple list return of array from API fetch

--- frontend/src/components/Links.tsx
+++ frontend/src/components/Links.tsx
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Links(){
     function loadLinks(){
-        console.info("Grabbed links...")
+        console.info("Grabbing links:")
         fetch("http://127.0.0.1:8080/links")
             .then((response) => response.json())
-            .then((data) => console.log(data));
-    }
+            .then((data) => {
+                let linkObject = data.data.links
+                const linksArray = linkObject.map((link: any) => 
+                    <li key={link.Id}> {link.name}</li>);
+                
+                console.debug(linksArray);
+                
+                setLinks(linksArray)
+            });
+        }
 
-    useEffect(() => {loadLinks()});
+    const [links, setLinks] = useState([]);
+    useEffect(() => {loadLinks(); }, []);
 
-    return <h1>link</h1>;
+    return <ul>{links}</ul>;
 }
 
 export default Links;
